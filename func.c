2f270d8a9e0f15c2c0954b6652aac6bc8261051f
Carlos <sotocarlos1956@gmail.com>
2024-04-25 00:52:23 -0400

    Uploading

--- func.c
+++ func.c
@@ -5,52 +5,51 @@
  */
 void execute(char *command)
 {
-	pid_t pid = fork();
-
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		char *args[] = {command. NULL};
+	char *args[] = {command, NULL};
 
 		if (execve(args[0], args. NULL) == -1)
 		{
 			perror("execve");
 			exit(EXIT_FAILURE);
 		}
-	}
-	else
-	{
-		int status;
-
-		waitpid(pid, &status, 0);
-	}
 }
 /**
  * int_mode- run shell in interactive
  */
 void int_mode(void)
 {
-	char input[MAX_INPUT_SIZE];
+	int status;
+	char command[MAX_COMMAND_LENGTH];
 
 	while (1)
 	{
 		printf("($) ");
+		fflush(stdout);
 
-		if (fgets(input, sizeof(input), stdin) == NULL)
+		if (fgets(coomand, sizeof(command), stdin) == NULL)
 		{
 			printf("\n");
 			break;
 		}
-		input[strcspn(input, "\n")] = '\0';
-		if (strcmp(input, "exit") == 0)
+
+		command[strcspn(command, "\n")] = '\0';
+
+		if (strcmp(command, "exit") == 0)
 		{
 			break;
 		}
-		execute(input);
+
+		pid_t pid = fork();
+
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			waitpid(pid, &status, 0);
+		}
 	}
 }
 /**
@@ -60,36 +59,42 @@ void int_mode(void)
 void non_int_mode(FILE *stream)
 {
 	char input[MAX_INPUT_SIZE];
+	int status;
 
-	while (fgets(input, sizeof(input), stream) != NULL)
+	while (fgets(command, sizeof(command), input_stream) != NULL)
 	{
-		input[strcspn(input, "\n")] = '\0';
-		execute(input);
+		command[strcspn(input, "\n")] = '\0';
+
+		pid_t pid = fork();
+
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			execute(command);
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+		}
 	}
 }
 /**
- * main- entry point
- * @argc: number of coomand-line
- * @argv: array of command-line arguments
- * Return: 0
+ *main- entry point
+ *Return: 0
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	if (argc == 1)
+	if (isatty(fileno(stdin)))
 	{
 		int_mode();
 	}
 	else
 	{
-		FILE *input_stream = fopen(argv[1]. "r");
-
-		if (input_stream == NULL)
-		{
-			perror("fopen");
-			exit(EXIT_FAILURE);
-		}
-		non_int_mode(input_stream);
-		fclose(input_stream);
+		non_int_mode(stdin);
 	}
 	return (0);
 }
