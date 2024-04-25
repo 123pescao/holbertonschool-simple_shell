46dcf634d23ca02a58edfefbf4926b65111c19b4
Carlos <sotocarlos1956@gmail.com>
2024-04-25 16:22:17 -0400

    still working on exercise #2

--- sshell.c
+++ sshell.c
@@ -1,4 +1,5 @@
 #include "head.h"
+#include "func.c"
 /**
  * main- Entry Point
  * Return: 0
@@ -9,7 +10,7 @@ int main(void)
 
 	while (1)
 	{
-		dprompt();
+		dprompt(void);
 
 		if (fgets(command, sizeof(command), stdin) == NULL)
 		{
