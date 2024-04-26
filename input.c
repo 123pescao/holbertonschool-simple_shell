b5a917268e40963ff2e01532a141cfe634a7d08c
Joel Caban <joel.caban2017@gmail.com>
2024-04-26 13:39:08 -0400

    Synt

--- input.c
+++ input.c
@@ -1,18 +1,31 @@
 #include "head.h"
-
-void read_input(int fd, char *buffer)
+/**
+ * read_command - Reads a command from standard input.
+ * @buffer: A character array where the read command will be stored.
+ * Description: This function reads a line from standard input using getline.
+ * Return: Returns 1 if a line is successfully read and stored,
+ *         or 0 if end-of-file is reached or a read error occurs.
+ */
+int read_command(char *buffer)
 {
-	int bytes_read = read(fd, buffer, BUFFER_SIZE - 1);
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
 
-	if (bytes_read > 0)
+	read = getline(&line, &len, stdin);
+
+	if (read == -1)
 	{
-		buffer[bytes_read] = '\0';
+		free(line);
+		return (0);
 	}
-	else if (bytes_read < 0)
+
+	if (line[read - 1] == '\n')
 	{
-		perror("read");
-		exit(EXIT_FAILURE);
+		line[read - 1] = '\0';
 	}
+
+	strcpy(buffer, line);
+	free(line);
+	return (1);
 }
-
-
