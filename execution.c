7d375e9dc6203e376097b989db21e0c67ee11c06
Joel Caban <joel.caban2017@gmail.com>
2024-04-26 13:34:42 -0400

    Synt

--- execution.c
+++ execution.c
@@ -1,74 +1,68 @@
 #include "head.h"
+#include "head.h"
 /**
- * process_input - Processes the input string for execution.
- * @input: String containing the entire input to be parsed.
+ * process_input - Executes the command provided in the input string.
+ * @input: String containing the command to be executed.
  *
- * Description: Takes a raw input string, parses it into arguments,
- *              and executes the command if valid.
+ * Description: This function takes a raw input string, which is expected
+ * to be a path to an executable file. It attempts to execute the command
+ * using execve. If execve fails, it prints an appropriate error message.
+ * The function assumes that no arguments are to be passed to the command
+ * and that the environment will be minimal.
+ *
+ * Return: void. If an error occurs during execution, an error message is
+ * printed to stderr using perror.
  */
 void process_input(char *input)
 {
-	char *args[MAX_ARGS];
-
-	parse_command(input, args);
-
-	if (args[0])
+	if (input && input[0])
 	{
-		execute_command(args);
+		char *argv[2];
+		char *envp[] = { NULL };
+
+		argv[0] = input;
+		argv[1] = NULL;
+
+		if (execve(input, argv, envp) == -1)
+		{
+			perror("Error executing command");
+		}
 	}
 }
+
+
 /**
- * execute_command - Executes a command given an array of arguments.
- * @args: Array of strings where the first is the command and the rest
- *        are the arguments to that command.
+ * execute_command - Executes a command in a child process.
+ * @command: The command to execute.
  *
- * Description: Forks the current process and attempts to execute the
- *              command specified in args[0] with execve. The parent
- *              waits for the execution to finish.
+ * Description: Forks new process, attempts to execute command using execve.
+ * The parent process waits for the execution to finish before continuing.
  */
-void execute_command(char *args[])
+void execute_command(char *command)
 {
 	pid_t pid;
 	int status;
+	char *argv[2];
+
+	argv[0] = command;
+	argv[1] = NULL;
 
 	pid = fork();
 
 	if (pid == 0)
 	{
-		if (execve(args[0], args, environ) == -1)
+		if (execve(command, argv, environ) == -1)
 		{
 			perror("execve");
 			exit(EXIT_FAILURE);
 		}
 	}
-	else if (pid > 0)
+	else if (pid < 0)
 	{
-		waitpid(pid, &status, 0);
+		perror("fork");
 	}
 	else
 	{
-		perror("fork");
-		exit(EXIT_FAILURE);
+		waitpid(pid, &status, 0);
 	}
-}
-/**
- * parse_command - Parses a command line into an array of arguments.
- * @command: The command line string to parse.
- * @args: The array to populate with arguments.
- *
- * Description: Splits the command string into individual words,
- *              using space, tab, and newline as delimiters. The
- *              array of arguments is null-terminated.
- */
-void parse_command(char *command, char *args[])
-{
-	int i = 0;
-
-	args[i] = strtok(command, " \t\n");
-
-	while (args[i] != NULL && i < MAX_ARGS - 1)
-	{
-		args[++i] = strtok(NULL, " \t\n");
-	}
-	args[i] = NULL;
 }
