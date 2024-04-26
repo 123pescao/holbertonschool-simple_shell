bd954bc90a4099ca4cbdbb25a34a5e2be4fedebb
Joel Caban <joel.caban2017@gmail.com>
2024-04-26 13:38:13 -0400

    Synt

--- head.h
+++ head.h
@@ -3,21 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
-#define MAX_ARGS 64
 
 extern char **environ;
 
-void int_mode(void);
+void execute_command(char *command);
+void loop_shell(void);
+int read_command(char *buffer);
 void process_input(char *input);
-void execute_command(char *args[]);
-void non_int_mode(FILE *stream);
 void read_input(int fd, char *buffer);
-void parse_command(char *command, char *args[]);
 
 #endif
